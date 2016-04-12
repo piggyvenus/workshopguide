4e381254bcc0b509a7aea20f65840725a8b886bd
Shanna Chan <shchan@redhat.com>
2016-04-12 09:53:45 -0700

    updated

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ var express = require('express');
 
 var app = express();
 
-app.use('/', express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 app.listen(8080, function() { console.log('listening')});
