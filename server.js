ae839c5b90051454e5ff58380a0ecc19687477ca
Shanna Chan <shchan@redhat.com>
2016-04-12 10:02:44 -0700

    update

--- server.js
+++ server.js
@@ -5,4 +5,19 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
+
+var public_dir = './public';
+
+app.get('/setup', function(req,res){
+	res.sendfile(public_dir + 'setup.html')
+});
+
+app.get('/appDockerfile', function(req,res){
+	res.sendfile(public_dir + 'appDockerfile.html')
+});
+
+app.get('/dockerImage', function(req,res){
+	res.sendfile(public_dir + 'dockerImage.html')
+});
+
 app.listen(8080, function() { console.log('listening')});
