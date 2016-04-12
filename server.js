4a6ff745c39e32265773b28940490a7df88e1f62
Shanna Chan <shchan@redhat.com>
2016-04-12 10:57:09 -0700

    updates

--- server.js
+++ server.js
@@ -22,4 +22,29 @@ app.get('/dockerImage', function(req,res){
 	res.sendfile(public_dir + 'dockerImage.html')
 });
 
+app.get('/binaryDeployment', function(req,res){
+	res.sendfile(public_dir + 'binaryDeployment.html')
+});
+
+
+app.get('/S2I-jboss', function(req,res){
+	res.sendfile(public_dir + 'S2I-jboss.html')
+});
+
+app.get('/S2I-datablubase', function(req,res){
+	res.sendfile(public_dir + 'S2I-datablubase.html')
+});
+
+app.get('/S2I-codechanges', function(req,res){
+	res.sendfile(public_dir + 'S2I-codechanges.html')
+});
+
+app.get('/bluegreen', function(req,res){
+	res.sendfile(public_dir + 'bluegreen.html')
+});
+
+app.get('/codePromotion', function(req,res){
+	res.sendfile(public_dir + 'codePromotion.html')
+});
+
 app.listen(8080, function() { console.log('listening')});
